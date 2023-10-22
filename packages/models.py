752d6923fd7066d6d09d09ea34dd7764bbcb7800
moufid-magdy <moufidmagdy.mm@gmail.com>
2023-10-22 19:57:11 +0300

    formating packages

--- packages/models.py
+++ packages/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Package (models.Model):
+
+class Package(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- packages/views.py
+++ packages/views.py
@@ -5,28 +5,33 @@ from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from .models import Package
 
+
 @csrf_exempt
 @require_http_methods(["POST"])
 def add_package(request):
     try:
-       data = json.loads(request.body)
-       package_name = data.get('name', '')
-       if Package.objects.filter(name=package_name).exists():
-            return JsonResponse({'error': 'A package with the same name already exists'}, status=400)
-       price = data.get('price','')
-       if not price.replace(".","",1).isdigit():
-           return JsonResponse({'error': "invalid price must be an integer "}, status=400)
+        data = json.loads(request.body)
+        package_name = data.get("name", "")
+        if Package.objects.filter(name=package_name).exists():
+            return JsonResponse(
+                {"error": "A package with the same name already exists"}, status=400
+            )
+        price = data.get("price", "")
+        if not price.replace(".", "", 1).isdigit():
+            return JsonResponse(
+                {"error": "invalid price must be an integer "}, status=400
+            )
 
-       package = Package(
-            name=data['name'],
-            description=data.get('description', ''),
-            price=data['price'],
+        package = Package(
+            name=data["name"],
+            description=data.get("description", ""),
+            price=data["price"],
         )
-       package.save()
-       return JsonResponse({'message': 'Package created successfully'})
+        package.save()
+        return JsonResponse({"message": "Package created successfully"})
     except json.JSONDecodeError as e:
-        return JsonResponse({'error': 'Invalid JSON data'}, status=400)
-    
+        return JsonResponse({"error": "Invalid JSON data"}, status=400)
+
 
 @csrf_exempt
 @require_http_methods(["GET"])
@@ -35,13 +40,13 @@ def get_all_packages(request):
     package_list = []
     for package in packages:
         package_data = {
-            'id': package.id,
-            'name': package.name,
-            'description': package.description,
-            'price': package.price,
+            "id": package.id,
+            "name": package.name,
+            "description": package.description,
+            "price": package.price,
         }
         package_list.append(package_data)
-    return JsonResponse({'packages': package_list})
+    return JsonResponse({"packages": package_list})
 
 
 @csrf_exempt
@@ -50,47 +55,45 @@ def get_package_by_id(request, package_id):
     try:
         package = Package.objects.get(pk=package_id)
         package_data = {
-            'id': package.id,
-            'name': package.name,
-            'description': package.description,
-            'price': package.price,
+            "id": package.id,
+            "name": package.name,
+            "description": package.description,
+            "price": package.price,
         }
         return JsonResponse(package_data)
     except Package.DoesNotExist:
-        return JsonResponse({'error': 'Package not found'}, status=404)
-    
+        return JsonResponse({"error": "Package not found"}, status=404)
+
 
 @csrf_exempt
 @require_http_methods(["PUT"])
 def update_package(request, package_id):
     try:
         package = Package.objects.get(pk=package_id)
-        data = json.loads(request.body.decode('utf-8'))
+        data = json.loads(request.body.decode("utf-8"))
 
         # Update package attributes
-        package.name = data.get('name', package.name)
-        package.description = data.get('description', package.description)
-        package.price = data.get('price', package.price)
+        package.name = data.get("name", package.name)
+        package.description = data.get("description", package.description)
+        package.price = data.get("price", package.price)
         package.save()
 
-        return JsonResponse({'message': 'Package updated successfully'})
+        return JsonResponse({"message": "Package updated successfully"})
     except Package.DoesNotExist:
-        return JsonResponse({'error': 'Package not found'}, status=404)
+        return JsonResponse({"error": "Package not found"}, status=404)
     except json.JSONDecodeError as e:
-        return JsonResponse({'error': 'Invalid JSON data'}, status=400)
-    
+        return JsonResponse({"error": "Invalid JSON data"}, status=400)
 
 
-    
 @csrf_exempt
 @require_http_methods(["DELETE"])
 def delete_package(request, package_id):
     try:
         package = Package.objects.get(pk=package_id)
         package.delete()
-        return JsonResponse({'message': 'Package deleted successfully'})
+        return JsonResponse({"message": "Package deleted successfully"})
     except Package.DoesNotExist:
-        return JsonResponse({'error': 'Package not found'}, status=404)
-# Create your views here.
+        return JsonResponse({"error": "Package not found"}, status=404)
+
 
 # Create your views here.
