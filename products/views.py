e98bba14870f2ae9f7e48f8c4c2d33c8cb4b7532
moufid-magdy <moufidmagdy.mm@gmail.com>
2023-11-09 15:35:44 +0200

    products' functions

--- products/views.py
+++ products/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
 
-# Create your views here.
+# Add a new product
+@api_view(["POST"])
+def addProduct(request):
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid():
+        product = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Edit an existing product by ID
+@api_view(["PUT"])
+def editProduct(request, product_id):
+    try:
+        product = Product.objects.get(pk=product_id)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Delete a product by ID
+@api_view(["DELETE"])
+def deleteProduct(request, product_id):
+    try:
+        product = Product.objects.get(pk=product_id)
+        product.delete()
+        return Response({'message': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+# Get a product by ID
+@api_view(["GET"])
+def getProductById(request, product_id):
+    try:
+        product = Product.objects.get(pk=product_id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+# Get all products
+@api_view(["GET"])
+def getAllProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
