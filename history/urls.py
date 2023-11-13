71b3c592c4df750b89e96f34f2c6b4d17b4575b4
farid <faridramy2003@gmail.com>
2023-11-13 23:21:43 +0200

    finish history

--- history/urls.py
+++ history/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from .views import history_create, list_histories
+from .views import history_create, list_histories, delete_history, get_history, update_history
 
 urlpatterns = [
     path('create', history_create, name='history_create'),
     path('', list_histories, name='list_histories'),
-
-    # path('<int:history_id>', history_detail, name='history_detail'),
-    # path('filter', filter_history, name="filter_history"),
-    # path('<int:history_id>/delete', delete_history, name="delete_history"),
-    # path('check_in', check_in, name="check_in"),
-    # path('<int:history_id>/check_out', check_out, name="check_out"),
+    path('<int:history_id>', get_history, name='get_history'),
+    path('<int:history_id>/update', update_history, name="update_history"),
+    path('<int:history_id>/delete', delete_history, name="delete_history"),
 ]
--- history/views.py
+++ history/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from django.utils import timezone
+from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import HistorySerializer, CreateHistorySerializer
 from .models import History
 from users.models import User
 from branches.models import Branch
-from django.db.models import Q
 
+# /api/history/create
 @api_view(["POST"])
 def history_create(request):
     client = request.data.get('client')
@@ -31,85 +31,48 @@ def history_create(request):
     else:
         return Response(serializer.errors)
 
+# /api/history
 @api_view(["GET"])
 def list_histories(request):
     query_params = request.query_params
-    query = {}
+    query = Q()
+
     for key, value in query_params.items():
-        if not key or not value:
+        if not key:
             return Response({"error": "Invalid query parameter provided."})
-        query[key] = value
+
+        if value == "None" or value == 'null':
+            value = "None"
+            query &= Q(**{f"{key}__isnull": True})
+        else:
+            query &= Q(**{key: value})
 
     try:
-        histories = History.objects.filter(**query)
+        histories = History.objects.filter(query)
         serializer = HistorySerializer(histories, many=True)
         return Response(serializer.data)
     except Exception as e:
         return Response({"error": str(e)})
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# /api/history/<int:history_id>
 @api_view(["GET"])
-def history_detail(request, history_id):
+def get_history(request, history_id):
     history = get_object_or_404(History, id=history_id)
     serializer = HistorySerializer(history)
     return JsonResponse(serializer.data)
 
-@api_view(["GET"])
-def filter_history(request):
-    pass
-    # queryset = History.objects.all()
-
-    # check_in_time = request.query_params.get('check_in_time')
-    # if check_in_time:
-    #     queryset = queryset.filter(check_in_time__date=check_in_time)
-
-    # branch = request.query_params.get('branch')
-    # if branch:
-    #     queryset = queryset.filter(branch=branch)
-
-    # employee = request.query_params.get('employee')
-    # if employee:
-    #     queryset = queryset.filter(employee=employee)
-
-    # client = request.query_params.get('client')
-    # if client:
-    #     queryset = queryset.filter(client=client)
-
-    # check_out_time = request.query_params.get('check_out_time')
-    # if check_out_time:
-    #     queryset = queryset.exclude(Q(check_out_time__isnull=False))
-
-    # serializer = HistorySerializer(queryset, many=True)
-    # return Response(serializer.data)
-
+# /api/history/<int:history_id>/update
+@api_view(['PUT'])
+def update_history(request, history_id):
+    history = get_object_or_404(History, id=history_id)
+    serializer = CreateHistorySerializer(history, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
+    
+# /api/history/<int:history_id>/delete
 @api_view(["DELETE"])
 def delete_history(request, history_id):
     try:
@@ -118,42 +81,4 @@ def delete_history(request, history_id):
         return Response({'message': 'History deleted successfully'})
     except History.DoesNotExist:
         return Response({'error': 'History not found'})
-
-@api_view(["POST"])
-def check_in(request):
-    try:
-        client = request.data.get('client')
-        employee = request.data.get('employee')
-        branch = request.data.get('branch')
-
-        if not client or not User.objects.filter(pk=client).exists():
-            return Response({"error": "Invalid or missing client"})
-
-        if not employee or not User.objects.filter(pk=employee).exists():
-            return Response({"error": "Invalid or missing employee"})
-
-        if not branch or not Branch.objects.filter(pk=branch).exists():
-            return Response({"error": "Invalid or missing branch"})
-
-        instance = History(
-            client=User.objects.get(pk=client),
-            employee=User.objects.get(pk=employee),
-            branch=Branch.objects.get(pk=branch),
-        )
-
-        instance.save()
-        return Response({"message": "Instance added successfully"})
-    except Exception as e:
-        return Response({"error": str(e)})
-
-@api_view(["PUT"])
-def check_out(request, history_id):
-    history = get_object_or_404(History, pk=history_id)
-    if history.check_out_time is not None:
-        return JsonResponse({'error': 'History not found'})
-    history.check_out_time = timezone.now()
-    history.payment = request.data.get("payment")
-    history.save()
-    return JsonResponse({'message': 'Check-out successful'})
-
 
