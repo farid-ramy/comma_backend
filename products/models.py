c3f185411a02616670cd7e6562a91d53ac016050
farid <faridramy2003@gmail.com>
2023-11-22 12:06:43 +0200

    fix bug

--- products/models.py
+++ products/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 from branches.models import Branch
 
-
-
 class Product (models.Model):
         name = models.CharField(max_length=50)
         quantity= models.IntegerField()
         price = models.DecimalField(max_digits=7, decimal_places=2)
         branch = models.ForeignKey(Branch, on_delete=models.SET_DEFAULT, null=True, blank=True, default=None)
-      
+
         class Meta:
                 unique_together = ['branch', 'name']
-# Define a unique constraint on the combination of branch and name
 
 def __str__(self):
-       return self.name
+        return self.name
