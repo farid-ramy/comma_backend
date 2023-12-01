d20f7177fea3dd2fcd1c753cc46ddb73afa382b9
marina <marinarizkallahishak@gmail.com>
2023-12-02 00:11:12 +0200

    rooms is working

--- rooms/roomsViews.py
+++ rooms/roomsViews.py
@@ -1,23 +1,22 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Room
 from .serializers import RoomSerializer, CreateRoomSerializer
 
-@api_view(['POST'])
-class create_room(APIView):
-    def post(self, request, *args, **kwargs):
+# /api/rooms/create
+class CreateRoom(APIView):
+    def post(self, request):
         serializer = CreateRoomSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-class list_rooms(APIView):
-    def get(self, request, *args, **kwargs):
+# /api/rooms/get
+class ListRooms(APIView):
+    def get(self, request):
         query_params = request.query_params
         query = {}
         for key, value in query_params.items():
@@ -32,27 +31,26 @@ class list_rooms(APIView):
         except Exception as e:
             return Response({"error": str(e)})
 
-@api_view(['GET'])
-class get_room(APIView):
-    def get(self, request, room_id, *args, **kwargs):
+# /api/rooms/get/<int:room_id>
+class GetRoom(APIView):
+    def get(self, request, room_id):
         room = get_object_or_404(Room, id=room_id)
         serializer = RoomSerializer(room)
         return Response(serializer.data)
 
-@api_view(['PUT'])
-class update_room(APIView):
-    def put(self, request, room_id, *args, **kwargs):
+# /api/rooms/<int:room_id>/update
+class UpdateRoom(APIView):
+    def put(self, request, room_id):
         room = get_object_or_404(Room, id=room_id)
-        serializer = CreateRoomSerializer(room, data=request.data)
+        serializer = CreateRoomSerializer(instance=room, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+        return Response(serializer.errors)
 
-@api_view(['DELETE'])
-class delete_room(APIView):
-    def delete(self, request, room_id, *args, **kwargs):
+# /api/rooms/<int:room_id>/delete
+class DeleteRoom(APIView):
+    def delete(self, request, room_id):
         room = get_object_or_404(Room, id=room_id)
         room.delete()
-        return Response({'message': 'room deleted successfully'})
+        return Response({'message': 'Room deleted successfully'})
--- rooms/urls.py
+++ rooms/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .roomsViews import create_room, list_rooms, get_room, update_room, delete_room
+from .roomsViews import CreateRoom, ListRooms, GetRoom, UpdateRoom, DeleteRoom
 from .reservationViews import create_reservation,list_reservations,get_reservation,update_reservation,delete_reservation
 
 urlpatterns = [
-    path("create", create_room, name="create_room"),
-    path("", list_rooms, name="list_rooms"),
-    path("<int:room_id>", get_room, name="get_room"),
-    path("<int:room_id>/update", update_room, name="update_room"),
-    path("<int:room_id>/delete", delete_room, name="delete_room"),
+    path("create",  CreateRoom.as_view(), name="create_room"),
+    path(" ", ListRooms.as_view() ,name="list_rooms"),
+    path("<int:room_id>", GetRoom.as_view(), name="get_room"),
+    path("<int:room_id>/update", UpdateRoom.as_view(), name="update_room"),
+    path("<int:room_id>/delete", DeleteRoom.as_view(), name="delete_room"),
 
     path("reservation/create", create_reservation, name="create_reservation"),
     path("reservation/", list_reservations, name="list_reservations"),
