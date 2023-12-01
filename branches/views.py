de94de76ebcf224f2e66f26646d9907943b414cf
moufid-magdy <moufidmagdy.mm@gmail.com>
2023-12-01 17:45:33 +0200

    branches refactor

--- branches/views.py
+++ branches/views.py
@@ -29,18 +29,17 @@ class get_branch(APIView):
         return Response(serializer.data)
 
 # # /api/branches/<int:branch_id>/update
-# @api_view(['PUT'])
-# def update_branch(request, branch_id):
-#     branch = get_object_or_404(Branch, id=branch_id)
-#     serializer = CreateBranchSerializer(instance=branch, data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#     return Response(serializer.errors)
+class update_branch(APIView):
+    def put(self, request, branch_id):
+        branch = get_object_or_404(Branch, id=branch_id)
+        serializer = CreateBranchSerializer(instance=branch, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
-# # /api/branches/<int:branch_id>/delete
-# @api_view(['DELETE'])
-# def delete_branch(request, branch_id):
-#     branch = get_object_or_404(Branch, id=branch_id)
-#     branch.delete()
-#     return Response({'message': 'User deleted successfully'})
+class delete_branch(APIView):
+    def delete(self, request, branch_id):
+        branch = get_object_or_404(Branch, id=branch_id)
+        branch.delete()
+        return Response({'message': 'Branch deleted successfully'})
