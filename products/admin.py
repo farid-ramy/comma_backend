e68a21e313ec15c21b09d02f7f73d238ac3bb06f
marina <marinarizkallahishak@gmail.com>
2023-12-19 23:37:54 +0200

    eav model

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,7 @@
+# products/admin.py
 from django.contrib import admin
-from .models import Product
+from .models import Attribute, Entity, Product
 
+admin.site.register(Attribute)
+admin.site.register(Entity)
 admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,27 +1,34 @@
+# products/models.py
 from django.db import models
-from .attribute import Attribute
 from branches.models import Branch
-from .entity import Entity
 
-class Product(models.Model):
-    entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
-    quantity = models.IntegerField()
-    attributes = models.ManyToManyField(Attribute, through='ProductAttributeValue')
-    branch = models.ForeignKey(Branch, on_delete=models.SET_DEFAULT, null=True, blank=True, default=None)
+class Attribute(models.Model):
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
-
-class ProductAttributeValue(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
-    value = models.CharField(max_length=255)
-
-    class Meta:
-        unique_together = ['product', 'attribute']
+class Entity(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.product.name} - {self.attribute.name}: {self.value}"
+        return self.name
+
+class Product(models.Model):
+    entity_name = models.CharField(max_length=255)
+    attribute_name = models.CharField(max_length=50)
+    attribute_value = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.IntegerField()
+    branch = models.ForeignKey(Branch, on_delete=models.SET_DEFAULT, null=True, blank=True, default=None)
+
+    def save(self, *args, **kwargs):
+        # Resolve the entity and attribute based on names before saving
+        entity, _ = Entity.objects.get_or_create(name=self.entity_name)
+        attribute, _ = Attribute.objects.get_or_create(name=self.attribute_name)
+        self.entity_name = entity
+        self.attribute_name = attribute
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.entity_name} - {self.attribute_name} - {self.attribute_value}"
